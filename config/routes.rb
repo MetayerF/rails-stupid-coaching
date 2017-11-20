29de78afce9e62970ee99c2ab774d94e90b75108
Francois Metayer <metayerfa@gmail.com>
2017-11-20 13:55:55 +0100

    Studip coach done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# Rails.application.routes.draw do
+#   get 'questions/answer'
+
+#   get 'questions/ask'
+
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'answer', to: 'questions#answer'
+  get 'ask', to: 'questions#ask'
+
 end
